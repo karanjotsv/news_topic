cc210edb3827c4ac87365eca4792f79dd753e47d
karanjotsv <karanjotvilkhu@gmail.com>
2023-06-17 09:52:59 -0400

    fetch data script

--- data/build_dataset.py
+++ data/build_dataset.py
@@ -1,84 +1,87 @@
 import json
-import os
+import os, re
+
 import posixpath
-from urllib.parse import urlparse
+from urllib import parse
 
 import requests
 import unidecode
-from goose3 import Goose
 from tqdm import tqdm
+from goose3 import Goose
+
+IMG_PATH = "./images"
 
 
 def resolveComponents(url):
-    """
-     resolveComponents('http://www.example.com/foo/bar/../../baz/bux/')
-    'http://www.example.com/baz/bux/'
-     resolveComponents('http://www.example.com/some/path/../file.ext')
-    'http://www.example.com/some/file.ext'
-    """
+    parsed = parse.urlparse(url)
+    path = posixpath.normpath(parsed.path)
+    
+    if parsed.path.endswith("/"): path += "/"
+    clean = parsed._replace(path)
 
-    parsed = urlparse(url)
-    new_path = posixpath.normpath(parsed.path)
-    if parsed.path.endswith('/'):
-        # Compensate for issue1707768
-        new_path += '/'
-    cleaned = parsed._replace(path=new_path)
+    return clean.geturl()
 
-    return cleaned.geturl()
+
+def clean_text(txt):
+    txt = re.subn('[“,”]', '"', txt.strip())
+    txt = re.subn('[‘,’]', "'", txt)
+
+    return txt
 
 
 def clean_article(item, article):
-    def clean_text(text):
-        text = text.strip()
-        text = text.replace('“', '"').replace('”', '"')
-        text = text.replace("‘", "'").replace("’", "'")
-        return text
+    abstract, caption, article = item['abstract'], item['caption'], clean_text(article)
 
-    abstract = item['abstract']
-    caption = item['caption']
-    article = clean_text(article)
-
-    tips = 'As a subscriber, you have 10 gift articles to give each month. Anyone can read what you share.'
-    article_cleaned = article.replace(abstract, '').replace(caption, '').replace(tips, '')
+    tips = "As a subscriber, you have 10 gift articles to give each month. Anyone can read what you share."
+    article = article.replace(abstract, "").replace(caption, "").replace(tips, "")
+    
     while True:
-        if article_cleaned.startswith('\n'):
-            article_cleaned = article_cleaned[1:]
-        else:
-            break
+        if article.startswith("\n"): article = clean_article[1 : ]
+        else: break
 
-    return article_cleaned
-
-
-def download_image(item):
-    img_data = requests.get(item['image_url'], stream=True).content
-    with open(os.path.join("./images/%s.jpg" % (item['image_id'])), 'wb') as f:
-        f.write(img_data)
-
-
-def download_article(item):
-    url = resolveComponents(item['article_url'])
-    extract = g.extract(url=url)
-    article = unidecode.unidecode(extract.cleaned_text)
     return article
 
 
-def del_dir(path):
-    for i in os.listdir(path):  # os.listdir(path_data)#返回一个列表，里面是当前目录下面的所有东西的相对路径
-        file_data = path + '/' + i  # 当前文件夹的下面的所有东西的绝对路径
-        if os.path.isfile(file_data):  # os.path.isfile判断是否为文件,如果是文件,就删除.如果是文件夹.递归给del_file.
-            os.remove(file_data)
+def get_image(item):  # to fetch image
+    img = requests.get(item['image_url'], stream=True).content
+    
+    with open(os.path.join(IMG_PATH, "%s.jpg" % (item['image_id'])), 'wb') as f:
+        f.write(img)
+
+
+def get_text(item):  # to fetch text
+    data = goose.extract(url=resolveComponents(item['article_url']))
+    text = unidecode.unidecode(data.cleaned_text)
+    
+    return text
+
+
+def check_dir(dir_path):  # to check if ./images exists and is empty
+    if os.path.exists(dir_path):
+        for file_name in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, file_name)
+            
+            try:
+                if os.path.isfile(file_path):  
+                    os.remove(file_path)
+            except Exception as e:
+                print("Failed to delete %s. %s" % (file_path, e))
+    
+    else:
+        os.mkdir(dir_path)
 
 
 if __name__ == '__main__':
-    g = Goose()
-    if os.path.exists('./images'):
-        del_dir('./images')
-    else:
-        os.makedirs('./images')
-    j = json.load(open('nytimes_dataset.json', 'r', encoding='utf8'))
-    new_j = []
-    for item in tqdm(j):
-        download_image(item)
-        item['body'] = clean_article(item, download_article(item))
-        new_j.append(item)
-    json.dump(new_j, open('nytimes_dataset_full.json', 'w', encoding='utf8'))
+    goose = Goose()
+    
+    check_dir(IMG_PATH)
+    meta_data, data = json.load(open("nytimes_metadata.json", 'r', encoding='utf8')), []
+    
+    for i in tqdm(meta_data):
+        try:
+            get_image(i)
+            i['body'] = clean_article(i, get_text(i))
+            data.append(i)
+        except Exception as e: pass
+
+    json.dump(data, open("nytimes_data.json", 'w', encoding='utf8'))
